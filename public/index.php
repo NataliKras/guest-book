54b5c31b1bc5821fbc5f4b58ab82e58170ff4a85
Natali <45793398+Sergey16091992@users.noreply.github.com>
2019-08-29 20:53:58 +0300

    добавлены: App, Src(MV(layouts)C, Core), Autoload.php, templaces

--- public/index.php
+++ public/index.php
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
+<?php 
+    // $arr=array(1,2,3,4,5);
+    // $reverse=array_reverse($arr);
+    // print_r ($reverse);
     
+    require_once "../src/Autoload.php";
+    use App\Core\Router;
 
-Guest book!!
-
-
-
-<h1>welcom!<h1> 
-    <u>publish content.<u>
-    <br><br><br>
-    <?php 
-    $arr=array(1,2,3,4,5);
-    $reverse=array_reverse($arr);
-    print_r ($reverse);
-    
-    ?>
-    
-</body>
-</html>
+    $obj= new Router();
+    $obj->run();
+?>
---
+++ src/core/Router.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Core;
+
+use Error;
+
+class Router
+    function __construct($view)
+    {
+        
+    }
+    // {
+    //     $this->controllerName=(isset($_GET["t"] ?? 'site'). 'Controller';
+    //     $this->actionName='action'.($_GET["a"]?? 'home');
+    // }
+
+
+{
+    function Run()
+    {
+        // echo "Hello World!";
+
+        $className="App\\Controller\\($this->controllerName)";
+        if (class_exists($className)){
+            $MVC=new $className();
+
+            if (method_exists($MVC, $this->actionName)){
+                $MVC->{$this->actionName}();
+            }else{
+                //echo "нет такого метода: $this->methodName";
+                (new ErrorController())->natFoundAction($this->actionName);             
+            }
+        }else{
+            echo "нет такого класса: $this->controllerName";
+            // (new ErrorController())->natFoundController($this->controllerName);
+            }
+    // }else{
+    //     //echo "ошибка доступа";
+    //     (new ErrorController())->forbiddenController();
+}
+}
+
+?>
